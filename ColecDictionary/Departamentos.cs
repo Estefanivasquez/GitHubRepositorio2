10cab6839c79af2077e1ffd563f51cf4c4cf9b1a
Esmeralda Vasquez <estefaniesmeraldavasquez2@gmail.com>
2023-06-19 11:07:51 -0600

    versionCompleja de departamentos v1

---
+++ ColecDictionary/Departamentos.cs
@@ -0,0 +1,15 @@
+namespace Departamentos
+{
+    //clase que define un departamento con su hombre y poblacion
+    public class depto 
+    {
+        public string Name { get; set; }
+        public int population { get; set; }
+
+        public dapto{String n,int poblacion}
+        {
+            Name = n;
+            population = poblacion;
+        }
+    }
+}
--- ColecDictionary/Program.cs
+++ ColecDictionary/Program.cs
@@ -1,36 +1,38 @@
-﻿using System.Collections;
-//definicion e inicializacion de los arreglos en paralelo y el diccionario
-String[] Departamento = { "Boaco", "Carazo", "Chinandega", "Chontales", "Costa Caribe Norte", "Costa Caribe Sur", "Estelí", "Granada", "Jinotega", "León", "Madriz", "Managua", "Masaya", "Matagalpa", "Nueva Segovia", "Río San Juan", "Rivas" };
-int[] Poblacion = { 185013, 197139, 439906, 190863, 530586, 414543, 229866, 214317, 475630, 421050, 174744, 1546939, 391903, 593503, 271581, 135446, 182645 };
-Dictionary<string, int> diccionario = Departamento
-          .Zip(Poblacion, (k, v) => new { Clave = k, Valor = v })
-          .ToDictionary(x => x.Clave, x => x.Valor);
-<<<<<<< HEAD
-//ordenando el diccionario de mayor a menor
-var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-//fijando los nombres de los departamentos con menor y mayor poblacion
-string minDepkey = ordenado.First().Key;
-string maxDepkey = ordenado.Last().Key;
-=======
-//Ordenando el diccionario de mayor a menor
-var ordenado = diccionario.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
-//fijando los nombres de los departamentos con menor y mayor poblacion
-string minDepkey =ordenado.First().Key;
-string maxDepkey =ordenado.Last().Key;
->>>>>>> versionsimple
-//reasignacion de los arreglos en paralelo
-//mostrar el diccionario sin ordenar
-Console.WriteLine($"datos Desordenados");
-foreach (var item in diccionario)
-Console.WriteLine($"{item.Key,-20}==>{item.value,10:NO}");
-Console.WriteLine();
-//Ordenando con LINQ OrdeBy el diccionari
-Departamento = ordenado.Keys.ToArray();
-Poblacion = ordenado.Values.ToArray();
-//mostrar los arreglos ordenados de menor a mayor
-for (var i = 0; i < Poblacion.Length; i++)
-    Console.WriteLine($"{Departamento[i],20} ==> {Poblacion[i],10:N0}");
-//suma de toda la poblacion y nombre de mayor a menor
-Console.WriteLine($"Población General:{Poblacion.Sum():N0}");
-Console.WriteLine($"Departamento con mayor Población:{maxDepkey}");
-Console.WriteLine($"Departamento con menor Población:{minDepkey}");
+﻿//programa que permita visualizar los departamentos de nicaragua con
+//su cantidad poblacional 
+//Encuentra el mayor, menor, sumas y ordene los datos 
+using Departamentos;
+//Lista de departamentos
+List<depto> isDeptos= new List<>();
+{
+    new depto("Boaco",185013);
+    new depto("Carazo",197139);
+    new depto("Chinandega",439906);
+    new depto("Chontales",190863);
+    new depto("Costa Caribe Norte",530586);
+    new depto("Costa Caribe sur",414543);
+    new depto("Esteli",229866);
+    new depto("Granada",214317);
+    new depto("Jinotega",475630);
+    new depto("Leon",421050);
+    new depto("Madrid",174744);
+    new depto("Managua",1546939);
+    new depto("Masaya",391903);
+    new depto("Matagalpa",593503);
+    new depto("Nueva Segovia",271581);
+    new depto("Rio San Juan",135446);
+    new depto("Rivas",182645);
+}
+//Encontrar el mayor y el menor (poblacion)
+depto mindepto = IstDepto.OrderBy(d =>d.populacion).Fist();
+depto maxdepto = IstDepto.OrderBy(d =>d.populacion).Last();
+//Ordenar los departamentos 
+var DeptOrdenado =isDeptos.OrderBy(d =>d.populacion);
+//Mostrar departamentos ordenados
+foreach (var item in DepOrdenado);
+    Console.WriteLine($"{item.Name,-20}==>{item.population,10:NO}");
+//
+}
+}
+
+}
